22196fb622e6f5fc385cbab818a3df99abbd3294
Iulian Preda <iulian.octavian.preda@gmail.com>
2019-04-13 23:31:15 +0300

    Created the project, the models and the controllers with EntityFramework

---
+++ MultimediaManagement/Models/EntityFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultimediaManagement.Models
+{
+    public partial class EntityFile
+    {
+        public Guid Id { get; set; }
+        public string Extension { get; set; }
+        public Guid PlaceholderId { get; set; }
+        public string Data { get; set; }
+        public bool IsUrl { get; set; }
+
+        public virtual Placeholder Placeholder { get; set; }
+    }
+}
---
+++ MultimediaManagement/Models/Placeholder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultimediaManagement.Models
+{
+    public partial class Placeholder
+    {
+        public Placeholder()
+        {
+            EntityFile = new HashSet<EntityFile>();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Keywords { get; set; }
+        public Guid CollectionId { get; set; }
+        public string Data { get; set; }
+        public string Extension { get; set; }
+
+        public virtual Collection Collection { get; set; }
+        public virtual ICollection<EntityFile> EntityFile { get; set; }
+    }
+}
